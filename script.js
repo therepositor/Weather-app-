319d0c6189ba19cf27136c4983f5c1cb695d6ef3
Joseph <66221058+therepositor@users.noreply.github.com>
2020-12-10 22:40:57 +0100

    Add files via upload
    A weather app, tells weather forecast of any city in the world at real-time.

---
+++ script.js
@@ -0,0 +1,54 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+
+async function getWeatherByLocation(city)   {
+    const resp = await fetch(url(city), { origin: "cors"});
+    const respData = await resp.json();
+
+    // console.log(respData, KelvinToCelsius(respData.main.temp));
+
+    addWeatherToPage(respData);
+}
+// getWeatherByLocation("istanbul"); 
+
+function addWeatherToPage(data) {
+    const temp = KelvinToCelsius(data.main.temp);
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML =     `
+
+    <h1>${search.value}</h1>
+    <h2> <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />${temp}°C <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" /></h2>
+    <small>${data.weather[0].main}</small>
+    `;
+
+    // clean main
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function KelvinToCelsius(K) {
+    return Math.floor(K - 273.15);
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const city = search.value;
+    console.log({ city });
+
+    if (city)   {
+        getWeatherByLocation(city);
+    }
+
+    console.log(getWeatherByLocation(city));
+})
